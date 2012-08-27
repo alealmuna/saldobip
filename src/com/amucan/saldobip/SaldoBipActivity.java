05e841666e734f450bec98f43b4f0d2bb8c879cf
Ale Almuna <malmuna@csrg.cl>
2012-08-27 00:05:34 -0400

    Add mockup data
    Add connection to database with mockup data on SaldoBipActivity.

--- src/com/amucan/saldobip/SaldoBipActivity.java
+++ src/com/amucan/saldobip/SaldoBipActivity.java
@@ -1,8 +1,14 @@
 package com.amucan.saldobip;
 
+import com.amucan.saldobip.db.DaoSession;
+import com.amucan.saldobip.db.DatabaseManager;
+import com.amucan.saldobip.db.Request;
+import com.amucan.saldobip.db.RequestDao;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,16 +20,24 @@ import android.support.v4.app.FragmentManager;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import java.util.Date;
+
 public class SaldoBipActivity extends FragmentActivity 
 {
   public final static String EXTRA_MESSAGE = "com.amucan.saldobip.MESSAGE";
   public String balance = "";
+	private DaoSession session;
+	private RequestDao requestDao;
+	private static String TAG = SaldoBipActivity.class.toString();
 
   @Override
   public void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
+		this.session = DatabaseManager.getDaoSession(this.getApplicationContext());
+    this.requestDao = this.session.getRequestDao();
+		this.addMockData();
   }
 
   private void showBalanceDialog(String balance) {
@@ -81,5 +95,19 @@ public class SaldoBipActivity extends FragmentActivity
     MenuInflater inflater = getMenuInflater();
     inflater.inflate(R.menu.main_menu, menu);
     return true;
+  }
+
+	private void addMockData(){
+		try{
+		  this.requestDao.deleteAll();
+      Request request = new Request(null,"123456","12:00",
+          -33.444518, -70.653664, 1500, new Date(),new Long(654321));
+      this.requestDao.insert(request);
+      request = new Request(null,"234567","12:00",
+          -33.444518, -70.653664, 2500, new Date(),new Long(765432));
+      this.requestDao.insert(request);
+		}catch(Exception e){
+			Log.e(TAG, e.getMessage());
+		}
   }
 }
