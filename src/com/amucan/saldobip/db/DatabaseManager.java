dfffa924492d163e5a656d06d3683c87a414501f
Ale Almuna <malmuna@csrg.cl>
2012-08-27 23:48:35 -0400

    Add DatabaseManager
    Database handler for retrieveng and saving request history.

---
+++ src/com/amucan/saldobip/db/DatabaseManager.java
@@ -0,0 +1,49 @@
+package com.amucan.saldobip.db;
+
+import com.amucan.saldobip.db.DaoMaster.DevOpenHelper;
+import com.amucan.saldobip.db.DaoSession;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.List;
+
+public class DatabaseManager {
+
+	private static DaoSession daoSession = null;
+  private RequestDao requestDao;
+	private DaoSession session;
+  private static DatabaseManager databaseManager = null;
+
+  private DatabaseManager(Context context){
+    this.session = this.getDaoSession(context);
+    this.requestDao = this.session.getRequestDao();
+    }
+
+	public static DaoSession getDaoSession(Context context){
+
+		if(daoSession == null){
+
+			DevOpenHelper helper = new DaoMaster.DevOpenHelper(context, "mayday-db", null);
+			SQLiteDatabase db = helper.getWritableDatabase();
+			DaoMaster daoMaster = new DaoMaster(db);
+			daoSession = daoMaster.newSession();
+		}
+		
+		return daoSession;
+	}
+
+	public static DatabaseManager getDBManagerInstance(Context context){
+
+		if(databaseManager == null){
+			databaseManager = new DatabaseManager(context);
+			return databaseManager;
+		}else{
+			return databaseManager;
+		}
+	}
+	
+	public List<Request> getAllRequests(){
+		return this.requestDao.loadAll();
+	}
+}
