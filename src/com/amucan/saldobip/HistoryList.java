030ac7adc2eceb0a1b1ea50884324cfd8b5af9ab
Ale Almuna <malmuna@csrg.cl>
2012-08-28 20:48:15 -0400

    Modify startup methods for adapeter handling
    onActivityCreated fill the request adapter.

--- src/com/amucan/saldobip/HistoryList.java
+++ src/com/amucan/saldobip/HistoryList.java
@@ -1,9 +1,7 @@
 package com.amucan.saldobip;
 
-import com.amucan.saldobip.db.DaoSession; 
 import com.amucan.saldobip.db.DatabaseManager;
 import com.amucan.saldobip.db.Request;
-//import com.amucan.saldobip.db.RequestDao;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
@@ -14,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
 import java.util.List;
 
 public class HistoryList extends ListFragment {
@@ -28,31 +27,31 @@ public class HistoryList extends ListFragment {
   }
   @Override
   public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    getActivity().setContentView(R.layout.history_list);
     Log.d(TAG, "onCreate");
-    this.databaseManager = DatabaseManager.getDBManagerInstance(
-        this.getActivity());
-    this.history = this.databaseManager.getAllRequests();
+    super.onCreate(savedInstanceState);
+  } 
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+    Log.d(TAG, "onCreateView");
+    View view = inflater.inflate(R.layout.history_list, container, false);
+    return view;
   }
-    
+
   @Override
   public void onActivityCreated(Bundle savedInstanceState) {
     Log.d(TAG, "onActivityCreated");
     super.onActivityCreated(savedInstanceState);
-    //this.mListView = (ListView) getView().findViewById(android.R.id.list);
+    this.databaseManager = DatabaseManager.getDBManagerInstance(
+        this.getActivity());
+    this.history = this.databaseManager.getAllRequests();
     Log.v(TAG, "Count: "+history.size());
-    //getActivity().setContentView(R.layout.history_list);
     this.adapter = new RequestAdapter(this.getActivity(),
         android.R.id.list,
         history, (long)history.size()); 
     setListAdapter(this.adapter); 
   }
-
-  @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
-      Bundle savedInstanceState) {
-    View view = inflater.inflate(R.layout.history_list, container, false);
-    return view;
-  }
+    
 }
